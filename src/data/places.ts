82de72ddcc5f18377cf2e3f81b79de134f61eada
JuditKaramazov <j.lazaromoyano7@gmail.com>
2024-04-25 13:29:41 +0200

    Update places.ts (data)

---
+++ src/data/places.ts
@@ -0,0 +1,66 @@
+import { IPin, EPinType } from 'src/lib/types'
+
+export const PLACES: IPin[] = [
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Home,
+    city: 'Engelhartszell an der Donau',
+    country: 'Austria',
+    coordinates: [48.5070, 13.7321],
+    date: '2021-12-03',
+    photo: '/photos/JuditKaramazov/engel.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Home,
+    city: 'Granollers',
+    country: 'Spain',
+    coordinates: [41.598167419433594, 2.283038854598999],
+    date: '2024-04-07',
+    photo: '/photos/JuditKaramazov/granollers.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Event,
+    title: 'Fête des Lumières',
+    city: 'Lyon',
+    country: 'France',
+    coordinates: [45.7640, 4.8357],
+    date: '2022-12-11',
+    photo: '/photos/JuditKaramazov/lyon-fete-lumieres.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Coffee,
+    title: 'Caffè il Forno',
+    city: 'Mataró',
+    country: 'Spain',
+    coordinates: [41.5381, 2.4447],
+    date: '2024-04-15',
+    photo: '/photos/JuditKaramazov/mataro.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Picture,
+    city: 'Roma',
+    country: 'Italy',
+    coordinates: [41.889048, 12.492502],
+    date: '2022-03-29',
+    photo: '/photos/JuditKaramazov/roma-colosseo.jpg'
+  },
+  {
+    author: 'Judit Lázaro',
+    username: 'JuditKaramazov',
+    type: EPinType.Picture,
+    city: 'Vienne',
+    country: 'France',
+    coordinates: [45.5256, 4.8743],
+    date: '2022-12-08',
+    photo: '/photos/JuditKaramazov/vienne-saint-maurice.jpg'
+  }
+]
